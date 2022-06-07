c92ea6c0a4edd0258d27374595baee787ee6163a
Walter Njabulo Manyathi <wn.manyathi@gmail.com>
2022-06-07 08:59:34 -0700

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,14 @@
-#include "School.s"
-
+#include "Alx.h"
 /**
- * main - program that prints School.
+ * main - Prints Alx
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n = 0;
-	char str_hbt[9] = "School";
-
-	while (n < 9)
-	{
-		_putchar(str_hbt[n]);
-		n += 1;
-	}
+	_putchar('A');
+	_putchar('l');
+	_putchar('x')
 	_putchar('\n');
 	return (0);
 }
