aca9c427cca28faeb1c57d206ae497d0cf2106ad
Walter Njabulo Manyathi <wn.manyathi@gmail.com>
2022-06-07 12:17:54 -0700

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /**
- * execute main 
- * main - entry point
- * return: Always 0 (Success)
+ * main - program that execute main.
+ *
+ * Return: Always 0 
  */
 
-int main()
-{ 	
-	for ( char c = 'A'; c <= 'Z'; c++)
-	{
-		putchar( c );
-	}
+int main(void)
+{
+	int n = 0;
+	char str_hbt[9] = "main";
 
-	return 0;
+	while (n < 9)
+	{
+		_putchar(str_hbt[n]);
+		n += 1;
+	}
+	_putchar('\n');
+	return (0);
 }
