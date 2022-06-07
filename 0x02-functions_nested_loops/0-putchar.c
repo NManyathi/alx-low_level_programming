0ee26d88111fb2b5dce91bd44755de91c208b96e
Walter Njabulo Manyathi <wn.manyathi@gmail.com>
2022-06-07 09:03:52 -0700

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-#include "Alx.h"
+#include "holberton.h"
 /**
- * main - Prints Alx
+ * main - Prints Holberton
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('A');
+	_putchar('H');
+	_putchar('o');
 	_putchar('l');
-	_putchar('x')
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
