a6619bd52a7c37436e78169eb712b97ae795e460
Walter Njabulo Manyathi <wn.manyathi@gmail.com>
2022-06-07 11:19:31 -0700

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,12 @@
  * return: Always 0 (Success)
  */
 
-int main(void)
-{	
-	char c[10] = "main";
-	int i = 0;
-	while (i < 9)
-}
-_putchar(c[i]);
-i++;
-{
-	_putchar('\n');
-	return (0);
+int main()
+{ 	
+	for ( char c = 'A'; c <= 'Z'; c++)
+	{
+		putchar( c );
+	}
+
+	return 0;
 }
