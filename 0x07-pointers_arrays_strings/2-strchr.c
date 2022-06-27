6041561f6f9bd58fe0bf7965c1a3c98c9a8511b8
Walter Njabulo Manyathi <wn.manyathi@gmail.com>
2022-06-27 02:29:21 -0700

    Task 2. strchr update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,20 @@
 *
 *Return: returns pointer to first occcurence of c
 */
+
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-		;
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+return (0);
 }
