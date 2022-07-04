ebc4206baac7a03b7e5b5d23a5658d39c5041591
Walter Njabulo Manyathi <wn.manyathi@gmail.com>
2022-07-04 04:08:14 -0700

    Task 4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,83 +1,63 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: malloced dog_t
- */
-
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	char *n, *o;
-	dog_t *new_dog = malloc(sizeof(dog_t));
-
-	if (!new_dog || !name || !owner)
-	{
-		return (NULL);
-	}
-	n = malloc(_strlen(name) + 1);
-	if (!n)
-	{
-		return (free(new_dog), NULL);
-	}
-	n = _strdup(name);
-	new_dog->name = n;
-	o = malloc(_strlen(owner) + 1);
-	if (!o)
-	{
-		return (free(new_dog->name), free(new_dog), NULL);
-	}
-	o = _strdup(owner);
-	new_dog->owner = o;
-	new_dog->age = age;
-	return (new_dog);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-	char *p = s;
-
-	while (*s)
-	{
-		s++;
-	}
-	return (s - p);
-}
-
-/**
- * _strdup - returns a pointer to a newly allocated space in memory,
+ * _strdup - returns a pointer to a newly allocated space in memory
  * which contains a copy of the string given as a parameter.
- * @str: string to be copied
- * Return: copied string
+ * @str: input char
+ * Return: char
  */
 
 char *_strdup(char *str)
 {
-	int i, len;
-	char *copy;
+	char *dup;
+	unsigned int i = 0;
+	unsigned int j = 0;
 
-	if (!str)
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i += 1;
+	i += 1;
+	dup = malloc(i * sizeof(*dup));
+	if (dup == NULL)
+		return (NULL);
+	while (j < i)
 	{
+		dup[j] = str[j];
+		j += 1;
+	}
+	return (dup);
+}
+
+/**
+ *new_dog - function that creates a new dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner:owner of dog
+ *Return: new struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = _strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
 		return (NULL);
 	}
-	len = _strlen(str);
-	copy = malloc(sizeof(char) * len + 1);
-	if (!copy)
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
+	if (new_dog->owner == NULL)
 	{
+		free(new_dog);
+		free(new_dog->name);
 		return (NULL);
 	}
-	for (i = 0; i < len; i++)
-	{
-		copy[i] = str[i];
-	}
-	copy[i] = 0;
-	return (copy);
+	return (new_dog);
 }
