ce256602aff4191bb328576c789e3e5a3b80e17e
Walter Njabulo Manyathi <wn.manyathi@gmail.com>
2022-07-19 02:59:43 -0700

    Task 0. Print list Update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
+
 /**
- * print_listint - prints all the elements of a listint_t list.
- * @h: pointer to the list.
- * Return: number of nodes.
- **/
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the list_t list.
+ *
+ * Return: The number of nodes in the list_t list.
+ */
 size_t print_listint(const listint_t *h)
 {
-const listint_t *linode = h;
-size_t incr = 0;
+	size_t nodes = 0;
 
-while (linode)
-{
-printf("%i\n", linode->n);
-incr++;
-linode = linode->next;
-}
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 
-return (incr);
+	return (nodes);
 }
